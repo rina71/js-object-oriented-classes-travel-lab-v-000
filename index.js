b0ed6255b218f8cf68585e0b1c6aba6566344803
Yurina Shin <yurinas@gmail.com>
2020-05-18 06:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ class Route {
   }
 
   blocksTravelled(){
-    return this.beginningLocation.vertical - this.endingLocation.vertical;
+    return this.endingLocation.vertical - this.beginningLocation.vertical;
   }
 }
