ebbb2e473cd67515d864fcd2d66c5ec44e492d5f
Yurina Shin <yurinas@gmail.com>
2020-05-18 07:50:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Route {
     return vertical + horizontal ;
   }
   estimatedTime(peak){
-    if (peak == true) {
+    if (arguments.length) {
       return this.blocksTravelled() / 3
     } else {
     return this.blocksTravelled() / 2
